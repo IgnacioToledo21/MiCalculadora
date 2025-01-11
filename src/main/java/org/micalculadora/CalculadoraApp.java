275f7c4515361f0ec7e13bd7ed4f8c5396235e30
IgnacioToledo21 <ignaciosurf124@gmail.com>
2025-01-11 14:04:26 +0000

    Estilos CSS
    Añadidos dos hojas de estilos css

--- src/main/java/org/micalculadora/CalculadoraApp.java
+++ src/main/java/org/micalculadora/CalculadoraApp.java
@@ -11,9 +11,17 @@ public class CalculadoraApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Mi calculadora");
+
+        // Cargar la vista principal desde el archivo FXML
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RootView.fxml"));
         Parent root = loader.load();
-        primaryStage.setScene(new Scene(root));
+
+        // Crear la escena y aplicar el CSS
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/css/classicstyles.css").toExternalForm());
+
+        // Configurar la escena en el escenario
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
--- src/main/java/org/micalculadora/CalculadoraController.java
+++ src/main/java/org/micalculadora/CalculadoraController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +15,64 @@ public class CalculadoraController implements Initializable{
     private final Calculadora calculadora = new Calculadora();
 
     @FXML
+    private Button button0;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    private Button button7;
+
+    @FXML
+    private Button button8;
+
+    @FXML
+    private Button button9;
+
+    @FXML
+    private Button buttonC;
+
+    @FXML
+    private Button buttonCE;
+
+    @FXML
+    private Button buttonComa;
+
+    @FXML
+    private Button buttonDividir;
+
+    @FXML
+    private Button buttonIgual;
+
+    @FXML
+    private Button buttonMultiplicar;
+
+    @FXML
+    private Button buttonRestar;
+
+    @FXML
+    private Button buttonSumar;
+
+    @FXML
     private TextField pantalla;
+
+    @FXML
+    private BorderPane root;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
